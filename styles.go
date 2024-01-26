4ddd006c3e961b3ee5ba0617968c9e4cab0860ed
Clara Smyth <106271826+ClaraSmyth@users.noreply.github.com>
2024-01-26 22:29:19 +0000

    Updated styles

--- styles.go
+++ styles.go
@@ -6,10 +6,25 @@ type ListStyles struct {
 	Title      lipgloss.Style
 	Selected   lipgloss.Style
 	Unselected lipgloss.Style
+	NoItems    lipgloss.Style
+	StatusBar  lipgloss.Style
+	TitleBar   lipgloss.Style
 }
 
-var styles = ListStyles{
-	Title:      lipgloss.NewStyle().Background(lipgloss.Color("#000000")).Width(20),
-	Selected:   lipgloss.NewStyle().Foreground(lipgloss.Color("#A46060")),
-	Unselected: lipgloss.NewStyle().Foreground(lipgloss.Color("237")),
+type Styles struct {
+	BaseStyles    ListStyles
+	FocusedStyles ListStyles
+}
+
+var styles = Styles{
+	BaseStyles: ListStyles{
+		Title:      lipgloss.NewStyle().Background(lipgloss.Color("#000000")).Width(20),
+		Selected:   lipgloss.NewStyle().Foreground(lipgloss.Color("#A46060")),
+		Unselected: lipgloss.NewStyle().Foreground(lipgloss.Color("237")),
+	},
+	FocusedStyles: ListStyles{
+		Title:      lipgloss.NewStyle().Background(lipgloss.Color("#1B50CF")).Width(20),
+		Selected:   lipgloss.NewStyle().Foreground(lipgloss.Color("#F44336")),
+		Unselected: lipgloss.NewStyle().Foreground(lipgloss.Color("237")),
+	},
 }
