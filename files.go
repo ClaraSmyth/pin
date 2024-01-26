78602e4ea795c7b5baabc9960164cc1613fff520
Clara Smyth <106271826+ClaraSmyth@users.noreply.github.com>
2024-01-26 10:54:44 +0000

    Refactor + Added GitCloneThemes

--- files.go
+++ files.go
@@ -4,6 +4,7 @@ import (
 	"cmp"
 	"io/fs"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"slices"
 	"strings"
@@ -79,32 +80,32 @@ func GetApps() []list.Item {
 }
 
 func CreateApp(newApp App, appList []list.Item) tea.Cmd {
-	if newApp.Name == "" {
-		return nil
-	}
-
-	appList = append(appList, newApp)
-	appsMap := make(map[string]App)
-
-	for _, item := range appList {
-		app := item.(App)
-		appsMap[app.Name] = app
-	}
-
-	WriteAppData(appsMap)
-
-	err := os.Mkdir(("./config/templates/" + newApp.Name), os.ModePerm)
-	if err != nil {
-		panic(err)
-	}
-
-	slices.SortFunc[[]list.Item, list.Item](appList, func(a, b list.Item) int {
-		return cmp.Compare(a.(App).Name, b.(App).Name)
-	})
-
-	newAppTemplates := []list.Item{}
-
 	return func() tea.Msg {
+		if newApp.Name == "" {
+			return nil
+		}
+
+		appList = append(appList, newApp)
+		appsMap := make(map[string]App)
+
+		for _, item := range appList {
+			app := item.(App)
+			appsMap[app.Name] = app
+		}
+
+		WriteAppData(appsMap)
+
+		err := os.Mkdir(("./config/templates/" + newApp.Name), os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+
+		slices.SortFunc[[]list.Item, list.Item](appList, func(a, b list.Item) int {
+			return cmp.Compare(a.(App).Name, b.(App).Name)
+		})
+
+		newAppTemplates := []list.Item{}
+
 		return updateAppListMsg{
 			appListItems:      appList,
 			templateListItems: newAppTemplates,
@@ -113,42 +114,41 @@ func CreateApp(newApp App, appList []list.Item) tea.Cmd {
 }
 
 func EditApp(newApp App, prevApp App, prevList []list.Item) tea.Cmd {
-
-	newList := []list.Item{newApp}
-	appsMap := make(map[string]App)
-	appsMap[newApp.Name] = newApp
-
-	for _, item := range prevList {
-		app := item.(App)
-
-		if app.Name == prevApp.Name {
-			continue
-		}
-
-		appsMap[app.Name] = app
-		newList = append(newList, app)
-	}
-
-	WriteAppData(appsMap)
-
-	if newApp.Name != prevApp.Name {
-		basePath := "./config/templates/"
-		prevPath := basePath + prevApp.Name
-		newPath := basePath + newApp.Name
-
-		err := os.Rename(prevPath, newPath)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	newAppTemplates := GetTemplates(newApp.Name)
-
-	slices.SortFunc[[]list.Item, list.Item](newList, func(a, b list.Item) int {
-		return cmp.Compare(a.(App).Name, b.(App).Name)
-	})
-
 	return func() tea.Msg {
+		newList := []list.Item{newApp}
+		appsMap := make(map[string]App)
+		appsMap[newApp.Name] = newApp
+
+		for _, item := range prevList {
+			app := item.(App)
+
+			if app.Name == prevApp.Name {
+				continue
+			}
+
+			appsMap[app.Name] = app
+			newList = append(newList, app)
+		}
+
+		WriteAppData(appsMap)
+
+		if newApp.Name != prevApp.Name {
+			basePath := "./config/templates/"
+			prevPath := basePath + prevApp.Name
+			newPath := basePath + newApp.Name
+
+			err := os.Rename(prevPath, newPath)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		newAppTemplates := GetTemplates(newApp.Name)
+
+		slices.SortFunc[[]list.Item, list.Item](newList, func(a, b list.Item) int {
+			return cmp.Compare(a.(App).Name, b.(App).Name)
+		})
+
 		return updateAppListMsg{
 			appListItems:      newList,
 			templateListItems: newAppTemplates,
@@ -157,35 +157,33 @@ func EditApp(newApp App, prevApp App, prevList []list.Item) tea.Cmd {
 }
 
 func DeleteApp(prevApp App, prevList []list.Item) tea.Cmd {
+	return func() tea.Msg {
+		newList := []list.Item{}
+		appsMap := make(map[string]App)
 
-	newList := []list.Item{}
-	appsMap := make(map[string]App)
+		for _, item := range prevList {
+			app := item.(App)
 
-	for _, item := range prevList {
-		app := item.(App)
+			if app.Name == prevApp.Name {
+				continue
+			}
 
-		if app.Name == prevApp.Name {
-			continue
+			appsMap[app.Name] = app
+			newList = append(newList, app)
 		}
 
-		appsMap[app.Name] = app
-		newList = append(newList, app)
-	}
+		WriteAppData(appsMap)
 
-	WriteAppData(appsMap)
+		err := os.RemoveAll("./config/templates/" + prevApp.Name)
+		if err != nil {
+			panic(err)
+		}
 
-	err := os.RemoveAll("./config/templates/" + prevApp.Name)
-	if err != nil {
-		panic(err)
-	}
+		newAppTemplates := []list.Item{}
 
-	newAppTemplates := []list.Item{}
-
-	slices.SortFunc[[]list.Item, list.Item](newList, func(a, b list.Item) int {
-		return cmp.Compare(a.(App).Name, b.(App).Name)
-	})
-
-	return func() tea.Msg {
+		slices.SortFunc[[]list.Item, list.Item](newList, func(a, b list.Item) int {
+			return cmp.Compare(a.(App).Name, b.(App).Name)
+		})
 		return updateAppListMsg{
 			appListItems:      newList,
 			templateListItems: newAppTemplates,
@@ -217,49 +215,58 @@ func GetTemplates(appName string) []list.Item {
 }
 
 func UpdateTemplates(appName string) tea.Cmd {
-	templates := GetTemplates(appName)
 	return func() tea.Msg {
+		templates := GetTemplates(appName)
 		return updateTemplateListMsg(templates)
 	}
 }
 
 func CreateTemplate(app App, filename string) tea.Cmd {
-	if filename == "" {
-		return nil
+	return func() tea.Msg {
+		if filename == "" {
+			return nil
+		}
+
+		f, err := os.Create("./config/templates/" + app.Name + "/" + filename)
+		if err != nil {
+			panic(err)
+		}
+
+		defer f.Close()
+
+		templates := GetTemplates(app.Name)
+		return updateTemplateListMsg(templates)
 	}
-
-	f, err := os.Create("./config/templates/" + app.Name + "/" + filename)
-	if err != nil {
-		panic(err)
-	}
-
-	defer f.Close()
-
-	return UpdateTemplates(app.Name)
 }
 
 func EditTemplate(app App, prevFilename string, newFilename string) tea.Cmd {
-	basePath := "./config/templates/"
-	prevPath := basePath + app.Name + "/" + prevFilename
-	newPath := basePath + app.Name + "/" + newFilename
+	return func() tea.Msg {
+		basePath := "./config/templates/"
+		prevPath := basePath + app.Name + "/" + prevFilename
+		newPath := basePath + app.Name + "/" + newFilename
 
-	err := os.Rename(prevPath, newPath)
-	if err != nil {
-		panic(err)
+		err := os.Rename(prevPath, newPath)
+		if err != nil {
+			panic(err)
+		}
+
+		templates := GetTemplates(app.Name)
+		return updateTemplateListMsg(templates)
 	}
-
-	return UpdateTemplates(app.Name)
 }
 
 func DeleteTemplate(app App, filename string) tea.Cmd {
-	basePath := "./config/templates/"
-	path := basePath + app.Name + "/" + filename
-	err := os.Remove(path)
-	if err != nil {
-		panic(err)
-	}
+	return func() tea.Msg {
+		basePath := "./config/templates/"
+		path := basePath + app.Name + "/" + filename
+		err := os.Remove(path)
+		if err != nil {
+			panic(err)
+		}
 
-	return UpdateTemplates(app.Name)
+		templates := GetTemplates(app.Name)
+		return updateTemplateListMsg(templates)
+	}
 }
 
 func GetThemes() []list.Item {
@@ -282,50 +289,71 @@ func GetThemes() []list.Item {
 }
 
 func CreateTheme(themeName string, themeList []list.Item) tea.Cmd {
-	basePath := "./config/schemes/custom/"
-
-	if themeName == "" {
-		return nil
-	}
-
-	f, err := os.Create(basePath + themeName + ".yaml")
-	if err != nil {
-		panic(err)
-	}
-
-	defer f.Close()
-
-	themeList = append(themeList, Theme{Name: themeName, Path: basePath + themeName + ".yaml"})
-
 	return func() tea.Msg {
+		basePath := "./config/schemes/custom/"
+
+		if themeName == "" {
+			return nil
+		}
+
+		f, err := os.Create(basePath + themeName + ".yaml")
+		if err != nil {
+			panic(err)
+		}
+
+		defer f.Close()
+
+		themeList := GetThemes()
+
 		return updateThemeListMsg(themeList)
 	}
 }
 
 func EditTheme(newThemeName string, prevTheme Theme) tea.Cmd {
-	basePath := "./config/schemes/custom/"
-
-	err := os.Rename(prevTheme.Path, basePath+newThemeName+".yaml")
-	if err != nil {
-		panic(err)
-	}
-
-	themeList := GetThemes()
-
 	return func() tea.Msg {
+		basePath := "./config/schemes/custom/"
+
+		err := os.Rename(prevTheme.Path, basePath+newThemeName+".yaml")
+		if err != nil {
+			panic(err)
+		}
+
+		themeList := GetThemes()
 		return updateThemeListMsg(themeList)
 	}
 }
 
 func DeleteTheme(theme Theme) tea.Cmd {
-	err := os.Remove(theme.Path)
-	if err != nil {
-		panic(err)
-	}
-
-	themeList := GetThemes()
-
 	return func() tea.Msg {
+		err := os.Remove(theme.Path)
+		if err != nil {
+			panic(err)
+		}
+
+		themeList := GetThemes()
+
+		return updateThemeListMsg(themeList)
+	}
+}
+
+func GitCloneSchemes() tea.Cmd {
+	return func() tea.Msg {
+		repo := "https://github.com/tinted-theming/schemes.git"
+		target := "./config/schemes/tinted-theming/"
+
+		err := os.RemoveAll(target)
+		if err != nil {
+			panic(err)
+		}
+
+		cmd := exec.Command("git", "clone", repo, target)
+		err = cmd.Run()
+		if err != nil {
+			panic(err)
+		}
+
+		themeList := GetThemes()
+
 		return updateThemeListMsg(themeList)
 	}
 }
