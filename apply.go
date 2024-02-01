ac157779ad0732e4a689a087a6aebf37155a5b2a
Clara Smyth <106271826+ClaraSmyth@users.noreply.github.com>
2024-02-01 13:30:32 +0000

    Split applyTheme into 2 funcs

--- apply.go
+++ apply.go
@@ -12,121 +12,124 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-func ApplyTheme(theme Theme) tea.Cmd {
+func ApplyThemeCmd(theme Theme) tea.Cmd {
 	return func() tea.Msg {
-		rawData, err := os.ReadFile(config.Apps)
-		if err != nil {
-			if errors.Is(err, os.ErrNotExist) {
-				err = os.WriteFile(config.ActiveTheme, []byte(theme.Path), 0666)
-				if err != nil {
-					panic(err)
-				}
+		applyTheme(theme)
+		return UpdateActiveStyles()
+	}
+}
 
-				return UpdateActiveStyles()
-			}
-			panic(err)
-		}
-
-		appsMap := make(map[string]App)
-
-		err = yaml.Unmarshal([]byte(rawData), &appsMap)
-		if err != nil {
-			panic(err)
-		}
-
-		themeData, err := os.ReadFile(theme.Path)
-		if err != nil {
-			panic(err)
-		}
-
-		scheme := builder.Scheme{}
-
-		err = yaml.Unmarshal([]byte(themeData), &scheme)
-		if err != nil {
-			panic(err)
-		}
-
-		for key, app := range appsMap {
-			if !app.Active || app.Path == "" || app.Template == "" {
-				app.Active = false
-				appsMap[key] = app
-				continue
-			}
-
-			templates, err := os.ReadDir(filepath.Join(config.Templates, app.Name))
-			if err != nil {
-				continue
-			}
-
-			var activeTemplatePath string
-
-			for _, template := range templates {
-				if filepath.Join(config.Templates, app.Name, template.Name()) == app.Template {
-					activeTemplatePath = app.Template
-				}
-
-				if strings.Split(template.Name(), ".")[0] == theme.Name {
-					activeTemplatePath = filepath.Join(config.Templates, app.Name, template.Name())
-					break
-				}
-			}
-
-			template, err := os.ReadFile(activeTemplatePath)
-			if err != nil {
-				app.Active = false
-				app.Template = ""
-				appsMap[key] = app
-				continue
-			}
-
-			data, err := builder.BuildTemplate(scheme, template)
+func applyTheme(theme Theme) {
+	rawData, err := os.ReadFile(config.Apps)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			err = os.WriteFile(config.ActiveTheme, []byte(theme.Path), 0666)
 			if err != nil {
 				panic(err)
 			}
 
-			if app.Rewrite {
-				err = os.WriteFile(app.Path, []byte(data), 0666)
-				if err != nil {
-					panic(err)
-				}
-			}
+			return
+		}
+		panic(err)
+	}
 
-			if !app.Rewrite {
-				configFileData, err := os.ReadFile(app.Path)
-				if err != nil {
-					app.Active = false
-					app.Path = ""
-					appsMap[key] = app
-					continue
-				}
+	appsMap := make(map[string]App)
 
-				updatedData := insertTemplate(string(configFileData), "START_PIN_HERE", "END_PIN_HERE", data)
+	err = yaml.Unmarshal([]byte(rawData), &appsMap)
+	if err != nil {
+		panic(err)
+	}
 
-				err = os.WriteFile(app.Path, []byte(strings.TrimSpace(updatedData)), 0666)
-				if err != nil {
-					panic(err)
-				}
-			}
+	themeData, err := os.ReadFile(theme.Path)
+	if err != nil {
+		panic(err)
+	}
 
-			if app.Hook != "" {
-				cmd := exec.Command("sh", "-c", app.Hook)
-				err = cmd.Run()
-				if err != nil {
-					panic(err)
-				}
-			}
+	scheme := builder.Scheme{}
 
+	err = yaml.Unmarshal([]byte(themeData), &scheme)
+	if err != nil {
+		panic(err)
+	}
+
+	for key, app := range appsMap {
+		if !app.Active || app.Path == "" || app.Template == "" {
+			app.Active = false
+			appsMap[key] = app
+			continue
 		}
 
-		err = os.WriteFile(config.ActiveTheme, []byte(theme.Path), 0666)
+		templates, err := os.ReadDir(filepath.Join(config.Templates, app.Name))
+		if err != nil {
+			continue
+		}
+
+		var activeTemplatePath string
+
+		for _, template := range templates {
+			if filepath.Join(config.Templates, app.Name, template.Name()) == app.Template {
+				activeTemplatePath = app.Template
+			}
+
+			if strings.Split(template.Name(), ".")[0] == theme.Name {
+				activeTemplatePath = filepath.Join(config.Templates, app.Name, template.Name())
+				break
+			}
+		}
+
+		template, err := os.ReadFile(activeTemplatePath)
+		if err != nil {
+			app.Active = false
+			app.Template = ""
+			appsMap[key] = app
+			continue
+		}
+
+		data, err := builder.BuildTemplate(scheme, template)
 		if err != nil {
 			panic(err)
 		}
 
-		WriteAppData(appsMap)
+		if app.Rewrite {
+			err = os.WriteFile(app.Path, []byte(data), 0666)
+			if err != nil {
+				panic(err)
+			}
+		}
 
-		return UpdateActiveStyles()
+		if !app.Rewrite {
+			configFileData, err := os.ReadFile(app.Path)
+			if err != nil {
+				app.Active = false
+				app.Path = ""
+				appsMap[key] = app
+				continue
+			}
+
+			updatedData := insertTemplate(string(configFileData), "START_PIN_HERE", "END_PIN_HERE", data)
+
+			err = os.WriteFile(app.Path, []byte(strings.TrimSpace(updatedData)), 0666)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		if app.Hook != "" {
+			cmd := exec.Command("sh", "-c", app.Hook)
+			err = cmd.Run()
+			if err != nil {
+				panic(err)
+			}
+		}
+
 	}
+
+	err = os.WriteFile(config.ActiveTheme, []byte(theme.Path), 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	WriteAppData(appsMap)
 }
 
 func insertTemplate(fileData, startString, endString, template string) string {
