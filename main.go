a9c10458f53d5148cb2d2ef4dc1c6abbdbd41ddd
Clara Smyth <106271826+ClaraSmyth@users.noreply.github.com>
2024-02-01 13:33:04 +0000

    Added ability to quick apply theme with cli

--- main.go
+++ main.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	"os"
+
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
+	args := os.Args[1:]
+
+	if len(args) > 0 {
+		themes := GetThemes()
+
+		if len(themes) == 0 {
+			return
+		}
+
+		for _, v := range themes {
+			theme := v.(Theme)
+
+			if theme.Name == args[0] {
+				applyTheme(theme)
+				return
+			}
+		}
+
+		return
+	}
+
 	m := newModel()
 
 	p := tea.NewProgram(m, tea.WithAltScreen())
